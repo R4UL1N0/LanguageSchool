8337e6e70b260972d6a6ae673f1e32e9b527c4ee
R4UL1N0 <raulino.storage@gmail.com>
2023-09-21 12:54:02 -0300

    spring boot template

---
+++ src/main/java/br/com/raulino/LanguageSchool/LanguageSchoolApplication.java
@@ -0,0 +1,13 @@
+package br.com.raulino.LanguageSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/raulino/LanguageSchool/LanguageSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.raulino.LanguageSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
